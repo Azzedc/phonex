a1c69a7bf5b797f8840b6b26c278501e565e4bb4
glevergeois <guillaume.levergeois@gmail.com>
2023-04-11 20:39:33 +0200

    update

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
 <footer class="footer_other">
     <div id="footer_id">
-        <a href="who_are_we.php">Who are we?</a>
-        <a href="sell.php">Sell</a>
+        <a href="who_are_we.php">Qui sommes-nous?</a>
+        <a href="sell.php">Revendre</a>
         <a href="support.php">Support</a>
-        <a href="legal_mentions.php">Legal mentions</a>
-        <a href="RGPD.php">RGPD</a>
+        <a href="legal_mentions.php">Mentions légales</a>
     </div>
 </footer>
--- legal_mentions.php
+++ legal_mentions.php
@@ -16,5 +16,33 @@ session_start();
 <?php require_once 'header.php';
 ?>
 
+<main>
+    <section class="recent_products">
+        <article class="rgpd">
+            <div class="content"> 
+                <p>
+                Cher utilisateur,<br>
+                <br>
+Nous prenons très au sérieux la protection de vos données personnelles et nous nous engageons à respecter la réglementation en vigueur en matière de protection des données, notamment le Règlement Général sur la Protection des Données (RGPD).<br>
+<br>
+Nous collectons uniquement les données nécessaires à la fourniture de nos services et nous ne les utilisons que dans le cadre de cette finalité. Les données que nous collectons comprennent votre nom, votre adresse email et votre mot de passe hashé.<br>
+<br>
+Nous conservons vos données personnelles pendant une durée limitée à 3 ans*. Passé ce délai, nous supprimons vos données de nos systèmes.<br>
+<br>
+Nous utilisons des mesures techniques et organisationnelles pour garantir la sécurité de vos données personnelles et nous nous engageons à respecter vos droits en matière de protection des données, tels que le droit d'accès, de rectification, de suppression et de portabilité de vos données.<br>
+<br>
+Si vous avez des questions ou des préoccupations concernant la protection de vos données personnelles, n'hésitez pas à nous contacter.<br>
+<br>
+Cordialement,<br>
+<br>
+L'équipe de PhoneX<br>
+<br>
+*Note : la durée de conservation mentionnée peut varier en fonction de la finalité du traitement de vos données.<br>
+                </p>
+            </div>
+        </article>    
+    </section>
+</main>
+
 <?php require_once 'footer.php';
 ?>
